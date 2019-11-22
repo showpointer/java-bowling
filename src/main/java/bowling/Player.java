9abe72f2f0439d4633eb1e86ee3b4ec86c485db5
임완호 <wanho.im@kt.com>
2019-11-22 22:48:49 +0900

    feat: 플레이서 생성

---
+++ src/main/java/bowling/Player.java
@@ -0,0 +1,40 @@
+package bowling;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Player {
+    private static String ENGLISH_REGEX = "^[a-zA-Z]*$";
+    private static int NAME_SIZE = 3;
+    private static Pattern pattern = Pattern.compile(ENGLISH_REGEX);
+
+    String name;
+
+    public Player(String name) {
+        checkEnglish(name);
+
+        if (name.length() != NAME_SIZE) {
+            throw new IllegalArgumentException("이름은 3글자여야 합니다");
+        }
+        this.name = name;
+    }
+
+    private void checkEnglish(String name) {
+        if (!pattern.matcher(name).matches()) {
+            throw new IllegalArgumentException("영어가 아닙니다");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/test/java/bowling/PlayerTest.java
@@ -0,0 +1,32 @@
+package bowling;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class PlayerTest {
+
+    @Test
+    void 플레이어_생성() {
+        Player player = new Player("PSJ");
+        assertThat(player).isEqualTo(new Player("PSJ"));
+    }
+
+    @Test
+    void 이름_영어인지_검사() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Player("한글사랑");
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"A", "BB", "DDDD"})
+    void 이름_3글자인지_검사(String name) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Player(name);
+        });
+    }
+}
